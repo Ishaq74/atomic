3cab6a65f590aee8fc1222b24625e96b17923107
Ishaq74 <ishaq.achour@gmail.com>
2026-08-23 21:03:14 +0200

    feat(services): register services module

--- src/lib/cms/bootstrap.ts
+++ src/lib/cms/bootstrap.ts
@@ -1,10 +1,12 @@
 import { registerModule } from "./module-registry";
 import { blogModule } from "@/modules/blog/module";
+import { servicesModule } from "@/modules/services/module";
 
 let bootstrapped = false;
 
 export function bootstrapModules(): void {
   if (bootstrapped) return;
   registerModule(blogModule);
+  registerModule(servicesModule);
   bootstrapped = true;
 }
