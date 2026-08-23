fd5ce234f4e5052bd5961bc8afd66a92c1b375bf
Ishaq74 <ishaq.achour@gmail.com>
2026-08-23 18:49:22 +0200

    refactor(blog): expose all canonical card projections

--- src/modules/blog/components/cards/index.ts
+++ src/modules/blog/components/cards/index.ts
@@ -1,1 +1,3 @@
 export { default as PostCard } from "@components/blog/PostCard.astro";
+export { default as PostCardGrid } from "@components/blog/cards/PostCardGrid.astro";
+export { default as PostCardHorizontal } from "@components/blog/cards/PostCardHorizontal.astro";
