24ed28e3468e548a574ae258f6ccc206c39eacad
Ishaq74 <ishaq.achour@gmail.com>
2026-08-23 18:45:36 +0200

    refactor(blog): establish single presentation boundary

---
+++ src/modules/blog/components/single/index.ts
@@ -0,0 +1,3 @@
+export { default as PostContent } from "@components/blog/PostContent.astro";
+export { default as AuthorCard } from "@components/blog/AuthorCard.astro";
+export { default as ArticleProgressBar } from "@components/blog/ArticleProgressBar.astro";
