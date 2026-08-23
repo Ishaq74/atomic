52fe2db7ebe297e19e0ca4bf439723241b8d7a3e
Ishaq74 <ishaq.achour@gmail.com>
2026-08-23 18:54:10 +0200

    refactor(cms): expose shared content editor boundary

--- src/core/content/index.ts
+++ src/core/content/index.ts
@@ -1,3 +1,5 @@
 export { sanitizeHtml } from "@/lib/sanitize";
 export * from "@/lib/content/editor-helpers";
 export * from "@/lib/content/internal-link-resolver";
+export { default as ContentEditor } from "@components/content/ContentEditor.astro";
+export { default as RichContent } from "@components/content/RichContent.astro";
