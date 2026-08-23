e7f28309d96d6197982dddf959f6126d62022cfa
Ishaq74 <ishaq.achour@gmail.com>
2026-08-23 18:44:36 +0200

    refactor(blog): expose canonical action boundary

---
+++ src/modules/blog/actions/index.ts
@@ -0,0 +1,7 @@
+export * from "@/actions/blog/post";
+export * from "@/actions/blog/category";
+export * from "@/actions/blog/tag";
+export * from "@/actions/blog/comment";
+export * from "@/actions/blog/review";
+export * from "@/actions/blog/reaction";
+export * from "@/actions/blog/notification";
