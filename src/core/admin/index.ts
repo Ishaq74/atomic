0891e5009a80584be2c4bbf0a6e0684bc5e14460
Ishaq74 <ishaq.achour@gmail.com>
2026-08-23 18:38:13 +0200

    refactor(cms): expose admin platform boundary

---
+++ src/core/admin/index.ts
@@ -0,0 +1,5 @@
+export { default as AdminResourceShell } from "@organisms/AdminResourceShell.astro";
+export { default as AdminFormShell } from "@organisms/AdminFormShell.astro";
+export { default as AdminResourceStats } from "@molecules/AdminResourceStats.astro";
+export { default as DataView } from "@molecules/DataView/DataView.astro";
+export type { AdminResourceDefinition, AdminManagementCapabilities } from "@/lib/cms/resource-contract";
