9eba87529eecef5124c6313af4057bfc06ad9526
Ishaq74 <ishaq.achour@gmail.com>
2026-08-23 18:50:37 +0200

    refactor(cms): expose admin list contract

--- src/core/admin/index.ts
+++ src/core/admin/index.ts
@@ -3,3 +3,4 @@ export { default as AdminFormShell } from "@organisms/AdminFormShell.astro";
 export { default as AdminResourceStats } from "@molecules/AdminResourceStats.astro";
 export { default as DataView } from "@molecules/DataView/DataView.astro";
 export type { AdminResourceDefinition, AdminManagementCapabilities } from "@/lib/cms/resource-contract";
+export * from "@/core/admin/filter-contract";
