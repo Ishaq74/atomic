d6a16396e522dfcef942ef486753f47ca85b411f
Ishaq74 <ishaq.achour@gmail.com>
2026-08-23 18:43:49 +0200

    fix(cms): expose real localization exports

--- src/core/localization/index.ts
+++ src/core/localization/index.ts
@@ -1,1 +1,8 @@
-export { LOCALES, DEFAULT_LOCALE, isLocale, type Locale } from "@/i18n/config";
+export {
+  LOCALES,
+  DEFAULT_LOCALE,
+  RTL_LOCALES,
+  LOCALE_LABELS,
+  type Locale,
+} from "@/i18n/config";
+export { isValidLocale } from "@/i18n/utils";
