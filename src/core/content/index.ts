e3837dfeb2fec9840d8b45c8f735601e4b357fa3
Ishaq74 <ishaq.achour@gmail.com>
2026-08-24 17:04:56 +0200

    feat(cms): export shared content text utilities

--- src/core/content/index.ts
+++ src/core/content/index.ts
@@ -1,5 +1,6 @@
 export { sanitizeHtml } from "@/lib/sanitize";
 export * from "@/lib/content/editor-helpers";
 export * from "@/lib/content/internal-link-resolver";
+export * from "./text";
 export { default as ContentEditor } from "@components/content/ContentEditor.astro";
 export { default as RichContent } from "@components/content/RichContent.astro";
