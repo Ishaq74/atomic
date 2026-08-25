478056bed89d1756c02079ee7dd5ea9c8e3507fb
Ishaq74 <ishaq.achour@gmail.com>
2026-08-25 20:19:39 +0200

    fix(services): export complete admin resource surface

--- src/modules/services/admin/index.ts
+++ src/modules/services/admin/index.ts
@@ -1,2 +1,9 @@
 export { serviceAdminResource } from "./resource";
-export { getServiceAdminData, getServiceAdminById, getServiceAdminStats } from "./loader";
+export {
+  getServiceAdminData,
+  getServiceAdminById,
+  getServiceAdminStats,
+  getServiceAdminTaxonomy,
+  getServiceModerationQueue,
+} from "./loader";
+export type { ServiceAdminFilters } from "./loader";
