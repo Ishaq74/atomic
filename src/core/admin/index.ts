f26a146aee43f573309dc7eacf1904745fe7932a
Ishaq74 <ishaq.achour@gmail.com>
2026-08-25 17:11:21 +0200

    refactor(core): make admin resource contract canonical

--- src/core/admin/index.ts
+++ src/core/admin/index.ts
@@ -2,5 +2,6 @@ export { default as AdminResourceShell } from "@organisms/AdminResourceShell.ast
 export { default as AdminFormShell } from "@organisms/AdminFormShell.astro";
 export { default as AdminResourceStats } from "@molecules/AdminResourceStats.astro";
 export { default as DataView } from "@molecules/DataView/DataView.astro";
-export type { AdminResourceDefinition, AdminManagementCapabilities } from "@/lib/cms/resource-contract";
+export type { AdminResourceDefinition, ResourceManagementCapabilities, ResourceActionCapabilities, ResourcePresentationVariants } from "./resource-contract";
+export { assertResourceCompatibility } from "./resource-contract";
 export * from "@/core/admin/filter-contract";
