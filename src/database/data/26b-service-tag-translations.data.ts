5ebf828b325bcc48b597510a01b5b8aeb6d15d9b
Ishaq74 <ishaq.achour@gmail.com>
2026-08-29 14:41:54 +0200

    seed(data): add localized Services tags

---
+++ src/database/data/26b-service-tag-translations.data.ts
@@ -0,0 +1,14 @@
+export default [
+  { id: "93100000-0000-0000-0000-000000000001", tagId: "93000000-0000-0000-0000-000000000001", organizationId: null, locale: "fr", name: "Lac", slug: "lac" },
+  { id: "93100000-0000-0000-0000-000000000002", tagId: "93000000-0000-0000-0000-000000000001", organizationId: null, locale: "en", name: "Lake", slug: "lake" },
+  { id: "93100000-0000-0000-0000-000000000003", tagId: "93000000-0000-0000-0000-000000000001", organizationId: null, locale: "es", name: "Lago", slug: "lago" },
+  { id: "93100000-0000-0000-0000-000000000004", tagId: "93000000-0000-0000-0000-000000000001", organizationId: null, locale: "ar", name: "بحيرة", slug: "buhayra" },
+  { id: "93100000-0000-0000-0000-000000000005", tagId: "93000000-0000-0000-0000-000000000002", organizationId: "f47ac10b-58cc-4372-a567-0e02b2c3d479", locale: "fr", name: "Culture", slug: "culture" },
+  { id: "93100000-0000-0000-0000-000000000006", tagId: "93000000-0000-0000-0000-000000000002", organizationId: "f47ac10b-58cc-4372-a567-0e02b2c3d479", locale: "en", name: "Culture", slug: "culture" },
+  { id: "93100000-0000-0000-0000-000000000007", tagId: "93000000-0000-0000-0000-000000000002", organizationId: "f47ac10b-58cc-4372-a567-0e02b2c3d479", locale: "es", name: "Cultura", slug: "cultura" },
+  { id: "93100000-0000-0000-0000-000000000008", tagId: "93000000-0000-0000-0000-000000000002", organizationId: "f47ac10b-58cc-4372-a567-0e02b2c3d479", locale: "ar", name: "ثقافة", slug: "thaqafa" },
+  { id: "93100000-0000-0000-0000-000000000009", tagId: "93000000-0000-0000-0000-000000000003", organizationId: "6ba7b810-9dad-11d1-80b4-00c04fd430c8", locale: "fr", name: "Outdoor", slug: "plein-air" },
+  { id: "93100000-0000-0000-0000-000000000010", tagId: "93000000-0000-0000-0000-000000000003", organizationId: "6ba7b810-9dad-11d1-80b4-00c04fd430c8", locale: "en", name: "Outdoor", slug: "outdoor" },
+  { id: "93100000-0000-0000-0000-000000000011", tagId: "93000000-0000-0000-0000-000000000003", organizationId: "6ba7b810-9dad-11d1-80b4-00c04fd430c8", locale: "es", name: "Aire libre", slug: "aire-libre" },
+  { id: "93100000-0000-0000-0000-000000000012", tagId: "93000000-0000-0000-0000-000000000003", organizationId: "6ba7b810-9dad-11d1-80b4-00c04fd430c8", locale: "ar", name: "أنشطة خارجية", slug: "anshita-kharijia" },
+];
