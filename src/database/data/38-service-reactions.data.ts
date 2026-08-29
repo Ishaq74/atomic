60746857fa979699760309f617aaa7d5b3da7269
Ishaq74 <ishaq.achour@gmail.com>
2026-08-29 14:44:25 +0200

    seed(data): add Services reactions

---
+++ src/database/data/38-service-reactions.data.ts
@@ -0,0 +1,4 @@
+export default [
+  { serviceId: "90000000-0000-0000-0000-000000000001", userId: "22222222-2222-2222-2222-222222222222", reactionType: "LIKE", createdAt: new Date("2026-02-24T12:00:00.000Z"), updatedAt: new Date("2026-02-24T12:00:00.000Z") },
+  { serviceId: "90000000-0000-0000-0000-000000000001", userId: "33333333-3333-3333-3333-333333333333", reactionType: "LOVE", createdAt: new Date("2026-02-24T12:05:00.000Z"), updatedAt: new Date("2026-02-24T12:05:00.000Z") },
+];
